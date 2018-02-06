ba0cabbcdcc1ba942a7c32b9cb954e3a496f6429
stla <laurent_step@yahoo.fr>
2018-02-06 04:13:54 +0100

    sphere mesh

---
+++ src/SphereMesh.hs
@@ -0,0 +1,94 @@
+module SphereMesh
+  where
+import           Data.IORef
+import           Graphics.Rendering.OpenGL.GL
+import           Graphics.UI.GLUT
+import           Utils.SphereMesh
+
+white,black,green :: Color4 GLfloat
+white = Color4 1   1   1   1
+black = Color4 0   0   0   1
+green = Color4 0   1   0   1
+
+display :: IORef GLfloat -> IORef GLfloat -> IORef GLdouble -> IORef Int
+        -> DisplayCallback
+display rot1 rot2 zoom iter = do
+  clear [ColorBuffer, DepthBuffer]
+  r1 <- get rot1
+  r2 <- get rot2
+  z <- get zoom
+  i <- get iter
+  let mesh = sphereMesh (max 0 i)
+  loadIdentity
+  (_, size) <- get viewport
+  resize z size
+  rotate r1 $ Vector3 1 0 0
+  rotate r2 $ Vector3 0 1 0
+  renderPrimitive Triangles $ do
+    materialDiffuse Front $= green
+    mapM_ drawTriangle mesh
+  swapBuffers
+  where
+    drawTriangle ((v1,v2,v3),n) = do
+      normal n
+      vertex v1
+      vertex v2
+      vertex v3
+
+resize :: Double -> Size -> IO ()
+resize zoom s@(Size w h) = do
+  viewport $= (Position 0 0, s)
+  matrixMode $= Projection
+  loadIdentity
+  perspective 45.0 (w'/h') 1.0 100.0
+  lookAt (Vertex3 0 0 (-3 + zoom)) (Vertex3 0 0 0) (Vector3 0 1 0)
+  matrixMode $= Modelview 0
+  where
+    w' = realToFrac w
+    h' = realToFrac h
+
+keyboardAndMouse :: IORef GLfloat -> IORef GLfloat -> IORef GLdouble
+                 -> IORef Int -> KeyboardMouseCallback
+keyboardAndMouse rot1 rot2 zoom iter key keyState _ _ =
+  case (key, keyState) of
+    (Char 'j', _)                   -> rot1 $~! subtract 1
+    (Char 'k', _)                   -> rot1 $~! (+1)
+    (Char 'h', _)                   -> rot2 $~! subtract 1
+    (Char 'l', _)                   -> rot2 $~! (+1)
+    (Char 'o', _)                   -> iter $~! subtract 1
+    (Char 'p', _)                   -> iter $~! (+1)
+    (Char 'q', _)                   -> leaveMainLoop
+    (MouseButton LeftButton, Down)  -> zoom $~! (+0.1)
+    (MouseButton RightButton, Down) -> zoom $~! subtract 0.1
+    _                               -> return ()
+
+idle :: IdleCallback
+idle = postRedisplay Nothing
+
+main :: IO ()
+main = do
+  _ <- getArgsAndInitialize
+  _ <- createWindow "Sphere mesh"
+  windowSize $= Size 600 600
+  initialDisplayMode $= [RGBAMode, DoubleBuffered, WithDepthBuffer]
+  clearColor $= white
+  materialAmbient Front $= Color4 0.5 0.5 0.5 1
+  materialShininess Front $= 3
+  lighting $= Enabled
+  light (Light 0) $= Enabled
+  position (Light 0) $= Vertex4 (-50) 0 (-100) 1
+  ambient (Light 0) $= black
+  diffuse (Light 0) $= white
+  specular (Light 0) $= black
+  depthFunc $= Just Lequal
+  depthMask $= Enabled
+  shadeModel $= Smooth
+  rot1 <- newIORef 0.0
+  rot2 <- newIORef 0.0
+  zoom <- newIORef 0.0
+  iter <- newIORef 0
+  displayCallback $= display rot1 rot2 zoom iter
+  reshapeCallback $= Just (resize 0)
+  keyboardMouseCallback $= Just (keyboardAndMouse rot1 rot2 zoom iter)
+  idleCallback $= Just idle
+  mainLoop
---
+++ src/Utils/SphereMesh.hs
@@ -0,0 +1,123 @@
+module Utils.SphereMesh
+  (sphereMesh)
+  where
+import           Data.Tuple.Extra             ((&&&), fst3)
+import           Graphics.Rendering.OpenGL.GL (Normal3 (..), Vertex3 (..))
+import           Linear                       (V3 (..), cross, signorm, (^+^),
+                                               (^-^), (^/), dot)
+
+type TriangleV3 = (V3 Double, V3 Double, V3 Double)
+type TriangleVx3 = (Vertex3 Double, Vertex3 Double, Vertex3 Double)
+
+splitTriangleV3 :: TriangleV3 -> [TriangleV3]
+splitTriangleV3 (a, b, c) = [tr1, tr2, tr3, tr4]
+  where
+    mab = signorm $ (a ^+^ b) ^/ 2
+    mac = signorm $ (a ^+^ c) ^/ 2
+    mbc = signorm $ (b ^+^ c) ^/ 2
+    tr1 = (a, mab, mac)
+    tr2 = (b, mbc, mab)
+    tr3 = (c, mac, mbc)
+    tr4 = (mab, mbc, mac)
+
+icosahedron :: [TriangleV3]
+icosahedron =
+  map (toTriangle . map (map (/ sqrt (1+phi*phi))))
+     [ [ [ 0.0 , 1.0 , -phi ]
+       , [ -phi , 0.0 , -1.0 ]
+       , [ -1.0 , phi , 0.0 ]
+       ] -- 1
+     , [ [ 0.0 , -1.0 , phi ]
+       , [ -1.0 , -phi , 0.0 ]
+       , [ 1.0 , -phi , 0.0 ]
+       ] -- 2
+     , [ [ 0.0 , -1.0 , phi ]
+       , [ -phi , 0.0 , 1.0 ]
+       , [ -1.0 , -phi , 0.0 ]
+       ] -- 3
+     , [ [ -phi , 0.0 , -1.0 ]
+       , [ -phi , 0.0 , 1.0 ]
+       , [ -1.0 , phi , 0.0 ]
+       ] -- 4
+     , [ [ -phi , 0.0 , -1.0 ]
+       , [ -1.0 , -phi , 0.0 ]
+       , [ -phi , 0.0 , 1.0 ]
+       ] -- 5
+     , [ [ 0.0 , 1.0 , phi ]
+       , [ -1.0 , phi , 0.0 ]
+       , [ -phi , 0.0 , 1.0 ]
+       ] -- 6
+     , [ [ 0.0 , -1.0 , phi ]
+       , [ 0.0 , 1.0 , phi ]
+       , [ -phi , 0.0 , 1.0 ]
+       ] -- 7
+     , [ [ 0.0 , 1.0 , -phi ]
+       , [ 1.0 , phi , 0.0 ]
+       , [ phi , 0.0 , -1.0 ]
+       ] -- 8
+     , [ [ 0.0 , 1.0 , phi ]
+       , [ 1.0 , phi , 0.0 ]
+       , [ -1.0 , phi , 0.0 ]
+       ] -- 9
+     , [ [ 0.0 , 1.0 , -phi ]
+       , [ -1.0 , phi , 0.0 ]
+       , [ 1.0 , phi , 0.0 ]
+       ] -- 10
+     , [ [ 0.0 , -1.0 , -phi ]
+       , [ -1.0 , -phi , 0.0 ]
+       , [ -phi , 0.0 , -1.0 ]
+       ] -- 11
+     , [ [ 0.0 , -1.0 , -phi ]
+       , [ 0.0 , 1.0 , -phi ]
+       , [ phi , 0.0 , -1.0 ]
+       ] -- 12
+     , [ [ 0.0 , -1.0 , -phi ]
+       , [ -phi , 0.0 , -1.0 ]
+       , [ 0.0 , 1.0 , -phi ]
+       ] -- 13
+     , [ [ 0.0 , -1.0 , -phi ]
+       , [ phi , 0.0 , -1.0 ]
+       , [ 1.0 , -phi , 0.0 ]
+       ] -- 14
+     , [ [ 0.0 , -1.0 , -phi ]
+       , [ 1.0 , -phi , 0.0 ]
+       , [ -1.0 , -phi , 0.0 ]
+       ] -- 15
+     , [ [ 0.0 , -1.0 , phi ]
+       , [ phi , 0.0 , 1.0 ]
+       , [ 0.0 , 1.0 , phi ]
+       ] -- 16
+     , [ [ 0.0 , -1.0 , phi ]
+       , [ 1.0 , -phi , 0.0 ]
+       , [ phi , 0.0 , 1.0 ]
+       ] -- 17
+     , [ [ phi , 0.0 , -1.0 ]
+       , [ phi , 0.0 , 1.0 ]
+       , [ 1.0 , -phi , 0.0 ]
+       ]
+     , [ [ phi , 0.0 , -1.0 ]
+       , [ 1.0 , phi , 0.0 ]
+       , [ phi , 0.0 , 1.0 ]
+       ]
+     , [ [ 0.0 , 1.0 , phi ]
+       , [ phi , 0.0 , 1.0 ]
+       , [ 1.0 , phi , 0.0 ]
+       ]
+     ]
+
+  where
+    phi = (1 + sqrt 5) / 2
+    toTriangle x = (toV3 (x!!0), toV3 (x!!1), toV3 (x!!2))
+    toV3 y = V3 (y!!0) (y!!1) (y!!2)
+
+sphereMesh' :: Int -> [TriangleV3]
+sphereMesh' n = iterate (concatMap splitTriangleV3) icosahedron !! n
+
+sphereMesh :: Int -> [(TriangleVx3, Normal3 Double)]
+sphereMesh n = map (toTriangleVx3 &&& normal) trianglesV3
+  where
+    trianglesV3 = sphereMesh' n
+    toTriangleVx3 (a, b, c) = (v3toVx3 a, v3toVx3 b, v3toVx3 c)
+    v3toVx3 (V3 x y z) = Vertex3 x y z
+    normal (a, b, c) = v3toN $ signorm $ cross (b ^-^ a) (c ^-^ a)
+    v3toN (V3 x y z) = Normal3 x y z

c1ee00c479aa5297ca5549cc4a68ac85701c4be1
stla <laurent_step@yahoo.fr>
2018-11-26 07:51:39 +0100

    test conemesh0

--- src/Cone/ConeMesh.hs
+++ src/Cone/ConeMesh.hs
@@ -1,7 +1,7 @@
 module Cone.ConeMesh
   where
 import qualified Data.Vector as V
-import           Data.Vector     ((!), fromList)
+import           Data.Vector     (Vector, fromList)
 
 cross :: Floating a => (a,a,a) -> (a,a,a) -> (a,a,a)
 cross (v1,v2,v3) (w1,w2,w3) =
@@ -16,15 +16,15 @@ nnormalize (x,y,z) = (-x/n,-y/n,-z/n)
   where
     n = sqrt(x*x+y*y+z*z)
 
-cmesh0 :: Floating a => a -> a -> a -> Int -> Int
-       -> ([(Vector (a,a,a), Vector (a,a,a))], [(Int,Int,Int,Int)])
-cmesh0 h rr r nstacks nslices = ((vertices, normals), quads)
+cmesh0 :: (Ord a, Floating a) => a -> a -> a -> Int -> Int
+       -> (Vector ((a,a,a),(a,a,a)), [(Int,Int,Int,Int)])
+cmesh0 h rr r nstacks nslices = (vertices_normals, quads)
   where
     h' = if r < rr then h else -h
     d = if r < rr then h' * r / rr else h' * rr / r
     hdivhmd = h' / (h' - d)
     -- grid --------------------------------------------------------------------
-    nstacks' = nstacks + 1;
+    -- nstacks' = nstacks + 1;
     -- i_ = V.enumFromN 0 nstacks
     -- u_ = V.map (\i -> ((h'-d)/realToFrac nstacks) * i) i_
     -- j_ = V.enumFromN 0 (nslices-1)
@@ -32,23 +32,23 @@ cmesh0 h rr r nstacks nslices = ((vertices, normals), quads)
     -- grid = [(u_ ! i, v ! j) |... vecteur...]
     i_ = [0 .. nstacks]
     j_ = [0 .. nslices-1]
-    u_ = [i * (h'-d)/realToFrac nstacks | i <- i_]
-    v_ = [j * 2 * pi / realToFrac nslices | j <- j_]
+    u_ = [fromIntegral i * (h'-d)/fromIntegral nstacks | i <- i_]
+    v_ = [fromIntegral j * 2 * pi / fromIntegral nslices | j <- j_]
     grid = [(u,v) | u <- u_, v <- v_]
     grid' = fromList grid
     gridij = [(i,j) | i <- tail i_, j <- j_]
     -- vertices ----------------------------------------------------------------
-    (vertices, normals) =
+    vertices_normals =
       V.map (\(u,v) -> let g = h' - u in
-                       let f = if r<rr then u * hdivhmd else u * hdivhmd - h in
+                       let f = if r<rr then u * hdivhmd else u * hdivhmd - h' in
                        let cosv = rr/h' * cos v in
-                       let sinv = rr/h' * sinv in
+                       let sinv = rr/h' * sin v in
                        let t1 = (-cosv, -sinv, hdivhmd) in
-                       let t2 = (-g * sinv, g*cos, 0) in
+                       let t2 = (-g * sinv, g*cosv, 0) in
                        ((g*cosv, g*sinv, f), nnormalize $ cross t1 t2)
             )
             grid'
     -- quads -----------------------------------------------------------------
     quads = map (\(i,j) -> let jp1 = if j<nslices-1 then j+1 else 0 in
-                           (i*nslices+j, (i-1)*nslices+j, (i-1)*nslices+jp1), i*nslices+jp1)
+                           (i*nslices+j, (i-1)*nslices+j, (i-1)*nslices+jp1, i*nslices+jp1))
                 gridij
---
+++ src/Cone/TestConeMesh0.hs
@@ -0,0 +1,97 @@
+module Cone.TestConeMesh0
+  where
+import           Data.Vector     (Vector, (!))
+import           Cone.ConeMesh
+import           Graphics.Rendering.OpenGL.GL
+import           Graphics.UI.GLUT
+import           Control.Monad                (forM_)
+
+-- cmesh0 :: Floating a => a -> a -> a -> Int -> Int
+--        -> ([(Vector (a,a,a), Vector (a,a,a))], [(Int,Int,Int,Int)])
+-- cmesh0 h rr r nstacks nslices = ((vertices, normals), quads)
+
+white,black,blue :: Color4 GLfloat
+white      = Color4    1    1    1    1
+black      = Color4    0    0    0    1
+blue       = Color4    0    0    1    1
+
+mesh :: (Vector ((Float,Float,Float),(Float,Float,Float)), [(Int,Int,Int,Int)])
+mesh = cmesh0 5 3 1 3 16
+
+verticesAndNormals :: Vector ((Float,Float,Float),(Float,Float,Float))
+verticesAndNormals = fst mesh
+
+toVx3 :: (Float,Float,Float) -> Vertex3 Float
+toVx3 (x,y,z) = Vertex3 x y z
+
+toN3 :: (Float,Float,Float) -> Normal3 Float
+toN3 (x,y,z) = Normal3 x y z
+
+quadIndices :: [(Int,Int,Int,Int)]
+quadIndices = snd mesh
+
+display :: DisplayCallback
+display = do
+  clear [ColorBuffer, DepthBuffer]
+  loadIdentity
+  forM_ quadIndices $ \(i,j,k,l) ->
+    renderPrimitive Quads $ do
+      materialDiffuse Front $= blue
+      drawQuad i j k l
+  swapBuffers
+  where
+    drawQuad i j k l = do 
+      normal ni
+      vertex vi
+      normal nj
+      vertex vj
+      normal nk
+      vertex vk
+      normal nl
+      vertex vl
+      where
+        (vi',ni') = verticesAndNormals ! i
+        (vj',nj') = verticesAndNormals ! j
+        (vk',nk') = verticesAndNormals ! k
+        (vl',nl') = verticesAndNormals ! l
+        vi = toVx3 vi'
+        ni = toN3 ni'
+        vj = toVx3 vj'
+        nj = toN3 nj'
+        vk = toVx3 vk'
+        nk = toN3 nk'
+        vl = toVx3 vl'
+        nl = toN3 nl'
+
+resize :: Size -> IO ()
+resize s@(Size w h) = do
+  viewport $= (Position 0 0, s)
+  matrixMode $= Projection
+  loadIdentity
+  perspective 45.0 (w'/h') 1.0 100.0
+  lookAt (Vertex3 2 2 13) (Vertex3 0 0 0) (Vector3 0 1 0)
+  matrixMode $= Modelview 0
+  where
+    w' = realToFrac w
+    h' = realToFrac h
+
+main :: IO ()
+main = do
+  _ <- getArgsAndInitialize
+  _ <- createWindow "Cone"
+  windowSize $= Size 500 500
+  initialDisplayMode $= [RGBAMode, DoubleBuffered, WithDepthBuffer]
+  clearColor $= white
+  materialAmbient Front $= black
+  lighting $= Enabled
+  light (Light 0) $= Enabled
+  position (Light 0) $= Vertex4 200 200 1300 1
+  ambient (Light 0) $= black
+  diffuse (Light 0) $= white
+  specular (Light 0) $= black
+  depthFunc $= Just Less
+  shadeModel $= Smooth
+  displayCallback $= display
+  reshapeCallback $= Just resize
+  idleCallback $= Nothing
+  mainLoop
